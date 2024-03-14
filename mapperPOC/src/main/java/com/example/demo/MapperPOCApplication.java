d1982fd9b7a6ab06a8d2c53928a5afbf7980aebb
raju kshirsagar <raju.kshirsagar151@gmail.com>
2024-03-14 14:45:40 +0530

    changed file name

---
+++ mapperPOC/src/main/java/com/example/demo/MapperPOCApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapperPOCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapperPOCApplication.class, args);
+	}
+
+	
+}
+
+
