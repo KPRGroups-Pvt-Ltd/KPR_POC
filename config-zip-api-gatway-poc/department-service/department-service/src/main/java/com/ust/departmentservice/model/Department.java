97e14ab45f631f4367b3cd742ac3f42713f948b4
sangharatna007 <sangharatna007@gmail.com>
2024-03-24 17:24:20 +0530

    config server , microservices and api gateway implenation

---
+++ config-zip-api-gatway-poc/department-service/department-service/src/main/java/com/ust/departmentservice/model/Department.java
@@ -0,0 +1,49 @@
+package com.ust.departmentservice.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Department {
+    private Long id;
+    private String name;
+    private List<Employee> list = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Employee> getList() {
+        return list;
+    }
+
+    public void setList(List<Employee> list) {
+        this.list = list;
+    }
+
+    public Department(Long id, String name, List<Employee> list) {
+        this.id = id;
+        this.name = name;
+        this.list = list;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", list=" + list +
+                '}';
+    }
+}
